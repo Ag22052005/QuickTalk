d0458622a1696bc90a151ca9411716b88cb0b94c
Ashish Gupta <ashish279338@gmail.com>
2024-12-30 02:37:17 +0530

    avatar page for mobile view

--- client/src/assets/screen/SelectAvatar.jsx
+++ client/src/assets/screen/SelectAvatar.jsx
@@ -43,12 +43,12 @@ const SelectAvatar = () => {
 
   return (
     <div className="w-screen h-screen flex justify-center items-center ">
-      <div className="border-2 w-[40%] h-[80%] flex py-4 items-center flex-col gap-8 overflow-hidden">
+      <div className="border-2 w-[40%] h-[80%] flex py-4 items-center flex-col gap-8 overflow-hidden avatar-box">
         <h1 className="font-extrabold text-2xl">Select Avatar</h1>
-        <div className="w-[50%] h-max-[90%] flex justify-center items-center">
+        <div className="w-[50%] h-max-[90%] flex justify-center items-center avatar-container">
           {
             avatars.length === 0 ? <div className="flex justify-center items-center"><Blocks/></div>:(
-            <ul className="flex flex-wrap justify-between gap-8">
+            <ul className="flex flex-wrap justify-between gap-8 avatar-ul">
             {
               avatars.map((avatar,index)=> (
                 <Link to ="/" key={index} >

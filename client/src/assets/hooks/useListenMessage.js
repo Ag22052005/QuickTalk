8ed6797005f4136201a919e92749872f52563ef4
Ashish Gupta <ashish279338@gmail.com>
2025-01-01 14:19:44 +0530

    added the chat msg notification audio

--- client/src/assets/hooks/useListenMessage.js
+++ client/src/assets/hooks/useListenMessage.js
@@ -1,21 +1,23 @@
-import { useContext, useEffect } from "react"
-import { SocketContext } from "../../context/SocketContextProvider"
-import { ChatContext } from "../../context/ChatContextProvider"
-
-
+import { useContext, useEffect } from "react";
+import { SocketContext } from "../../context/SocketContextProvider";
+import { ChatContext } from "../../context/ChatContextProvider";
+import chatmsgaudio from '../audios/chatmsgaudio.mp3'
 const useListenMessage = () => {
   const { socket } = useContext(SocketContext);
   const { currentConversation, setCurrentConversation } = useContext(ChatContext);
-
+  const audio = new Audio(chatmsgaudio);
+    function playAudio() {
+      audio.play();
+    }
   useEffect(() => {
-    socket?.on('newMessage', (newMessage) => {
-      setCurrentConversation(prev => [...prev, newMessage]);
-      console.log("listening to newMessage: ", newMessage);
+    socket?.on("newMessage", (newMessage) => {
+      setCurrentConversation((prev) => [...prev, newMessage]);
+      playAudio();
     });
 
     // Cleanup listener
-    return () => socket?.off('newMessage');
+    return () => socket?.off("newMessage");
   }, [socket, setCurrentConversation]);
 };
 
-export default useListenMessage
+export default useListenMessage;

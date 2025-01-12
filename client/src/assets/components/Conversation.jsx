63ddc1b43b2815fc839aa5abf5ed2b9c4affd6db
Ashish Gupta <ashish279338@gmail.com>
2025-01-12 17:43:46 +0530

    skeleton path error after name changing is fix

--- client/src/assets/components/Conversation.jsx
+++ client/src/assets/components/Conversation.jsx
@@ -25,7 +25,7 @@ import { FaArrowLeft } from "react-icons/fa6";
 import { motion } from "framer-motion";
 import EmojiPicker from "emoji-picker-react";
 import dp from "../images/dp.png";
-import MessageSkeleton from "../skeleton/messageSkeleton";
+import MessageSkeleton from "../skeleton/MessageSkeleton";
 
 
 const Conversation = () => {

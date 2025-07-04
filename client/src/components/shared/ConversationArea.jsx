67574f8b7daee85480142bd7a51af5efb541cbe5
Ashish Gupta <ashish279338@gmail.com>
2025-07-04 17:05:55 +0530

    alignment of chat is fixed

--- client/src/components/shared/ConversationArea.jsx
+++ client/src/components/shared/ConversationArea.jsx
@@ -9,7 +9,7 @@ function ConversationArea() {
   return (
     <div
       className={`flex flex-col h-full w-auto md:w-[65%] ml-1 rounded-lg shadow-lg overflow-hidden 
-        border border-muted bg-white dark:bg-zinc-900 transition-all duration-300 
+        border border-muted bg-white dark:bg-zinc-900 transition-all duration-300  w-full
         ${currentReceiver ? "" : "hidecontainer"}`}
     >
       {currentReceiver ? <Conversation /> : <Greeting />}
